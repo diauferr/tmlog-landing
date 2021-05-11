eef4023d3ac7e7dc7f95fe7d8a62dfa25a1a375d
DiAugust <diaugustdev@gmail.com>
2021-05-10 21:39:08 -0300

    vercel adapter configurado

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+const vercel = require(‘@sveltejs/adapter-vercel’);
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: vercel(),
 	}
 };
 
