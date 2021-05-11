d06675d1484d18dca1b001f27ea18b03b4ef119f
DiAugust <diaugustdev@gmail.com>
2021-05-10 22:54:09 -0300

    adicionado TailWind CSS com svelte-add

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,14 @@
+import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 
 import vercel from '@sveltejs/adapter-vercel';
 
 const config = {
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
