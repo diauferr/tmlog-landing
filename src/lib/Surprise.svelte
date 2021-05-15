8d7fbcfd39193d8313f8ffb9287925bcb9d135e0
DiAugust <diaugustdev@gmail.com>
2021-05-15 17:59:39 -0300

    alteração de texto do componente surprise

--- src/lib/Surprise.svelte
+++ src/lib/Surprise.svelte
@@ -63,7 +63,7 @@ fire(0.1, {
   
 <div class="surprise w-auto h-autoflex flex-col items-center justify-center">
   <h1 class="text-2xl lg:text-4xl text-center font-semibold">
-    🎉 {message} 🍾
+    🎉 {message}
   </h1>
   <p class="text-center font-normal">
     Agora é só aguardar que entraremos em contato com você.
