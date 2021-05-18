886dad4efb269b83bf186a5c594581020e5f8a84
DiAugust <diaugustdev@gmail.com>
2021-05-18 07:19:54 -0300

    novas alterações de fix no i18n

--- src/lib/Values.svelte
+++ src/lib/Values.svelte
@@ -9,10 +9,11 @@ $: dict.set(translations);
 
 <!-- <img src="/wavevalues.svg" alt="" class="w-full"> -->
 <div class="w-full h-auto flex flex-col items-center text-center px-8 md:px-28 lg:px-52" id="card">
+    <h1 class="text-2xl py-12 font-opensans">{$t('values_data')[0].heading}</h1>
     {#each $t('values_data')[0].values_list as { title, description, image_url }}
     <div class="flex flex-row items-center">
         <img src={image_url} alt="" class="" id="img">
-        <h1 class="pl-2 py-6 text-2xl font-semibold font-opensans uppercase">{$t('values_data')[0].heading}</h1>
+        <h1 class="pl-2 py-6 text-2xl font-semibold font-opensans uppercase">{title}</h1>
     </div>
     <p class="mb-4 p-8 rounded-md font-medium font-opensans">{description}</p>
     {/each}
